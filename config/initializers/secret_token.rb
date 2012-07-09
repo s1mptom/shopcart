501446db9d85b431687938b6b196bd2b587c80a8
s1mptom <s1mptom@mac-pro-pavel.dlink>
2012-07-09 17:57:30 +0400

    1st commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopcart::Application.config.secret_token = '28f24d0dc2b052d403364ffc9707ea0bc21946ed6b1a3e9e9c07ed6e3e9685b2c29d7413395d1d05596adbc2d91ee3851d7deb879dd50026d2532473b8534b9b'
